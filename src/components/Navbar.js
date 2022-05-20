cdf8c41794f1301e65653718bf3ee1c2589d8f78
Atikanajla <hasrifaaulia5@gmail.com>
2022-05-20 10:07:35 +0700

    -mengatur breaking point container -menambahkan container kedalam header

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,23 +10,25 @@ function Navbar() {
   // const {watchlist} = useGlobalContext();
 
   return <header className="header">
-    <div className="header__logo"><Link to="/">IMDb</Link></div>
-    <nav className="header__navbar">
-        <div className="btn menu-btn">
-            <FaBars/><span>Menu</span>
-        </div>
-        <div className="mega-menu"></div>
-    </nav>
-    <div className="header__search-bar">
-        <div className="search-filter">All <FaCaretDown /></div>
-        <input type="text"></input>
-        <button><FiSearch /></button>
-    </div>
-    <a href="" className="header__IMDbpro btn">IMDbpro</a>
-    <div className="header__user-menu">
-        <div className="btn h__watchlist"><Link to="watchlist"><BsBookmarkPlus /> Watchlist 0</Link></div>
-        <div className="btn h_sign-in">Sign In</div>
-        <div className="btn h_language">EN <FaCaretDown /></div>
+    <div className='container'>
+      <div className="header__logo"><Link to="/">IMDb</Link></div>
+      <nav className="header__navbar">
+          <div className="btn menu-btn">
+              <FaBars/><span>Menu</span>
+          </div>
+          <div className="mega-menu"></div>
+      </nav>
+      <div className="header__search-bar">
+          <div className="search-filter">All <FaCaretDown /></div>
+          <input type="text"></input>
+          <button><FiSearch /></button>
+      </div>
+      <a href="" className="header__IMDbpro btn">IMDbpro</a>
+      <div className="header__user-menu">
+          <div className="btn h__watchlist"><Link to="watchlist"><BsBookmarkPlus /> Watchlist 0</Link></div>
+          <div className="btn h_sign-in">Sign In</div>
+          <div className="btn h_language">EN <FaCaretDown /></div>
+      </div>
     </div>
 
   </header>
